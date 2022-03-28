6a234212d930111f7e7ae3a0b8d7d6f01c1f916c
Broctholumeu <Brocksqd@gmail.com>
2022-03-28 15:10:07 -0500

    Building api fetch and state management in React first

--- salty-sous-chef/src/App.js
+++ salty-sous-chef/src/App.js
@@ -1,10 +1,36 @@
+import React from 'react';
 import './App.css';
 import DropDown from './DropDown.js';
 import ButtonAction from './Button.js';
 import ConversionOutput from './ConversionOutput.js';
 import Stack from '@mui/material/Stack';
+import useEffect from 'react';
+import useState from 'react';
 
 function App() {
+  
+  // const [data, setData] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
+
+  // const BASE_URL = "http://data.fixer.io/api/latest"
+
+  // useEffect(() => {
+  //   fetch(BASE_URL)
+  //   .then((response) => {
+  //       if (!response.ok) {
+  //           throw new Error(
+  //               'This is an HTTP error: The status is ${response.status}'
+  //           );
+  //       }
+  //       return response.json();
+  //   })
+  //   .then((actualData) => console.log(actualData))
+  //   .catch((err) => {
+  //       console.log(err.message);
+  //   })
+  // }, [])
+
   return (
     <div className="App">
       <ConversionOutput />
